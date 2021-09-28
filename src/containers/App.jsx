4c7aab829140aa3f3af460f9af7ae631a0341120
Samuel Garcia <samfrontdev@gmail.com>
2021-09-27 20:37:19 -0500

    Initial setup

---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WISDOM</h1>
+    </div>
+  );
+}
+
+export default App;
