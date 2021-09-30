658a65de2b44320bfaa95c15520f44f0dba47812
Samuel Garcia <samfrontdev@gmail.com>
2021-09-29 19:58:41 -0500

    SearchBar input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-
+import "../assets/styles/SearchBar.css";
 export const message = "RESULTS";
 
 function SearchBar() {
+  const getValue = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>SEARCH</h1>
-    </div>
+    <section>
+      <h1>📚 Search a book</h1>
+      <input
+        id="input"
+        className="searchInput"
+        type="text"
+        placeholder="🔍 Search any book"
+        onChange={getValue}
+      />
+    </section>
   );
 }
 
